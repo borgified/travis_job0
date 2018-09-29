9bd352e93fbf345f0379abc39808c89dfd8591e7
borgified <borgified@gmail.com>
2018-09-29 18:15:29 +0000

    a

--- functions.sh
+++ functions.sh
@@ -1,25 +1,5 @@
 # helper functions
 
-function wait_for {
-  #poll every 5 seconds until upstream job is finished
-  local repo_slug=$1
-  local id=$2
-
-  while [ "$state" != "finished" ]; do
-    local result=$(curl -s -H "Travis-API-Version: 3" -H "User-Agent: API Explorer" -H "Authorization: token $TOKEN" https://api.travis-ci.com/repo/$repo_slug/request/$id)
-    local state=$(echo $result | jq -r .state)
-    local build_id=$(echo $result | jq -r .builds[0].id)
-    echo "waiting for upstream job (build_id: $build_id) to complete. its current state is: $state"
-    sleep 5
-  done
-
-  local final_result=$(curl -s -H "Travis-API-Version: 3" -H "User-Agent: API Explorer" -H "Authorization: token $TOKEN" https://api.travis-ci.com/repo/$repo_slug/request/$id)
-  local status=$(echo $final_result | jq -r .builds[0].state)
-
-  echo $status
-
-}
-
 function trigger_job {
 
   local repo_slug=$1
@@ -33,7 +13,11 @@ function trigger_job {
       "env": {
         "UPSTREAM_SHA": "$TRAVIS_PULL_REQUEST_SHA",
         "UPSTREAM_REPO": "$TRAVIS_PULL_REQUEST_SLUG"
-      }
+      },
+      "before_script": "STATE=pending ./update_build_status.sh",
+      "script": "./sleep.sh",
+      "after_success": "STATE=success ./update_build_status.sh",
+      "after_failure": "STATE=failure ./update_build_status.sh"
     }
   }
 }
